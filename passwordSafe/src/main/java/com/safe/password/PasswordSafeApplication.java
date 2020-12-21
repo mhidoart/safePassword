1fc6cbe7c901833cd6ab9ae477420d526a7ac719
Medhi Assabbane <medhi.assabbane@ensiie.fr>
2020-12-21 16:04:31 +0100

    first commit

---
+++ passwordSafe/src/main/java/com/safe/password/PasswordSafeApplication.java
@@ -0,0 +1,13 @@
+package com.safe.password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordSafeApplication.class, args);
+	}
+
+}
