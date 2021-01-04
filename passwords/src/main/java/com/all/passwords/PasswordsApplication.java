42554ecdc29e0e4d9b1cab44ed08d2924f71037c
ASSABBANE Mehdi <mhidoart@gmail.com>
2021-01-05 00:53:01 +0100

    fresh start

---
+++ passwords/src/main/java/com/all/passwords/PasswordsApplication.java
@@ -0,0 +1,13 @@
+package com.all.passwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordsApplication.class, args);
+	}
+
+}
